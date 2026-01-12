9717decc06c5a7455be40f843cd43ad9243670a4
Lord-Einstein <darylleinstein@gmail.com>
2026-01-12 11:15:39 +0100

    feat : add test-formatage-string.js

---
+++ 1. Fondamentaux/test-formatage-string.js
@@ -0,0 +1,5 @@
+const hello = "Hello";
+const world = "World.";
+
+const helloWorld = `${hello} ${world}`; //pratique pour éviter de laisser js créer des varibles intermédiaires pour concaténer la caîne finale.
+console.log(helloWorld);
